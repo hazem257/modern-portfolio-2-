3bb93dc491755a0529b942d068e89bf5735255eb
‪Hazem Gmall‬‏ <161766768+hazem257@users.noreply.github.com>
2025-06-13 14:45:18 +0300

    Update projects.js

--- public/projects.js
+++ public/projects.js
@@ -1,11 +1,12 @@
-export const myprojects = [
- { projectTitle: "Portfolio (1)", category: ["css","html"], about: "A Responsive Portfolio for personal use You can show Your Work in it.... ", imgpath: "", live: "", Github: "" },
- { projectTitle: "Portfolio (2)", category: ["css","html"], about: "A Responsive Portfolio for personal use You can show Your Work in it.  With some Addition....", imgpath: "", live: "", Github: "" },
- { projectTitle: "Electrical Store", category: ["css","html"], about: "FrontEnd Responsive Electric Store....", imgpath: "", live: "", Github: "" },
- { projectTitle: "Coffee Shop", category: ["css","html"], about: "FrontEnd Responsive coffee Shop....", imgpath: "", live: "", Github: "" },
- { projectTitle: "Ramadan Cards", category: ["css","html"], about: "", imgpath: "", live: "", Github: "" },
- { projectTitle: "Premier Market", category: ["react", "full stack"], about: "This is A System To Manage Market like Add & Show order and Client....  ", imgpath: "", live: "", Github: "" },
- { projectTitle: "Portfolio (3)", category: ["css","html"], about: "A Responsive Portfolio for personal use You can show Your Work in it With Many of Improvements ....", imgpath: "", live: "", Github: "" },
- { projectTitle: "Quraan Player", category: ["css","html"], about: " You Can Listen To Your Favourite Quran reader With many different recitations.....", imgpath: "", live: "", Github: "" },
+export const myprojects =[
+ { projectTitle: "Portfolio (2)", category: ["css","html"], about: "A Responsive Portfolio for personal use You can show Your Work in it With Many of Improvements .... ", imgpath: "./projects/1.png", live: "https://hazem11.vercel.app/", Github: "" },
+ { projectTitle: "Portfolio (3)", category: ["css","html"], about: "A Responsive Portfolio for personal use You can show Your Work in it.  With some Addition ....", imgpath: "./projects/6.png", live: "https://hazem-port-f.vercel.app/", Github: "" },
+ { projectTitle: "Electrical Store", category: ["css","html"], about: "FrontEnd Responsive Electric Store ....", imgpath: "./projects/10.png", live: "https://cars-nine-chi.vercel.app/", Github: "" },
+ { projectTitle: "Coffee Shop", category: ["css","html"], about: "FrontEnd Responsive coffee Shop ....", imgpath: "./projects/5.png", live: "https://hazem1.vercel.app/", Github: "" },
+ { projectTitle: "Ramadan Cards", category: ["css","html"], about: "You Can Generate Celebrateing card To Your Friend And Your Family Using his Name .... ", imgpath: "./projects/4.png", live: "https://ramadan-46h.vercel.app/", Github: "https://github.com/hazem257/ramadan-46h.git" },
+ { projectTitle: "Premier Market", category: ["react", "full stack"], about: "This is A System To Manage Market like Add & Show order and Client ....  ", imgpath: "./projects/9.PNG", live: "https://final2-ten.vercel.app/", Github: "https://github.com/hazem257/final2.git" },
+ { projectTitle: "Portfolio (1)", category: ["css","html"], about: "A Responsive Portfolio for personal use You can show Your Work in it ....", imgpath: "./projects/3.png", live: "https://h1-iota.vercel.app/", Github: "" },
+ { projectTitle: "Quraan Player", category: ["css","html"], about: " You Can Listen To Your Favourite Quran reader With many different recitations .....", imgpath: "./projects/q.PNG", live: "https://hazem11.vercel.app/_pages/index.html", Github: "" },
+ { projectTitle: "Modern Portfolio", category: ["react"], about: "A Responsive Portfolio for personal use You can show Your Work in it With Many of Improvements And Some Animation ....", imgpath: "./projects/11.png", live: "https://modern-portfolio-2-522x.vercel.app/", Github: "https://github.com/hazem257/modern-portfolio-2-.git" },
 
 ]
